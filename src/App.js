50d1bfce5f7f8cc17f559533965d4ba3b5b56f7c
7hourspg <rajiv840704@gmail.com>
2023-11-22 22:41:40 +0530

    use glb models

--- src/App.js
+++ src/App.js
@@ -1,98 +1,52 @@
 import {
-  CameraControls,
   Environment,
   OrbitControls,
+  OrthographicCamera,
   PerspectiveCamera
 } from '@react-three/drei'
-import { Canvas, useFrame } from '@react-three/fiber'
+import { Canvas } from '@react-three/fiber'
 import React from 'react'
-import { useRef } from 'react'
-import './App.css'
-
-const Sphere = () => {
-  const ref = useRef()
-
-  const isMobile = window.innerWidth < 768
-  console.log(isMobile)
-
-  useFrame((state, delta) => {
-    ref.current.position.x = Math.sin(state.clock.getElapsedTime()) * 2
-    ref.current.position.y = Math.cos(state.clock.getElapsedTime()) * 2
-  })
-
-  return (
-    <>
-      <mesh castShadow ref={ref}>
-        <sphereGeometry attach='geometry' args={[1, 50, 50]} />
-        <meshStandardMaterial color='#fff' />
-      </mesh>
-    </>
-  )
-}
+import Woman from './Woman'
+import Peoples from './Peoples'
 
 const Plane = () => {
   return (
-    <mesh position={[0, 0, -1]} receiveShadow>
-      <planeGeometry attach={'geometry'} args={[17, 17]} />
-      <meshStandardMaterial color='#87ceeb' />
+    <mesh rotation={[0, 0, 0]} position={[0, 0, -1]} receiveShadow>
+      <planeGeometry attach={'geometry'} args={[20, 90]} />
+      <meshStandardMaterial color='#88b719' />
     </mesh>
   )
 }
 
 function App () {
-  const cameraControlRef = useRef()
-  const DEG45 = Math.PI / 4
   return (
-    <>
-      <Canvas id='canvas' style={{ height: '100vh', width: '100vw' }} shadows>
-        <PerspectiveCamera makeDefault position={[0, -10, 0]} />
-        <Sphere />
+    <Canvas style={{ height: '100vh', width: '100vw' }} shadows>
+      <PerspectiveCamera makeDefault position={[0, -4, 2]} />
 
-        <Plane />
+      <ambientLight intensity={0.5} />
 
-        {/* <directionalLight position={[-4, -1, 2]} castShadow /> */}
-        <spotLight
-          args={['#ffff', 10, 0, Math.PI / 4.5, 0.5, 0]}
-          position={[-4, -1, 1]}
-          castShadow
-        />
-        <ambientLight intensity={0.5} />
-        <OrbitControls
-          maxPolarAngle={Math.PI / 2}
-          minPolarAngle={Math.PI / 2}
-        />
-        <CameraControls
-          ref={cameraControlRef}
-          maxPolarAngle={Math.PI / 2}
-          minPolarAngle={Math.PI / 2}
-        />
+      <spotLight
+        args={['#ffff', 10, 0, Math.PI / 4.5, 0.5, 0]}
+        position={[-4, -1, 1]}
+        castShadow
+      />
 
-        <Environment background>
-          <mesh>
-            <sphereGeometry args={[-5, -5, 0]} />
-            <meshBasicMaterial color='#87ceeb' />
-          </mesh>
-        </Environment>
-      </Canvas>
-      <div style={{ position: 'absolute', top: '0' }}>
-        <button
-          type='button'
-          onClick={() => {
-            cameraControlRef.current?.rotate(DEG45, DEG45, true)
-          }}
-        >
-          rotate theta 45deg
-        </button>
-        <button
-          type='button'
-          onClick={() => {
-            cameraControlRef.current?.reset(true)
-          }}
-        >
-          reset
-        </button>
-      </div>
-    </>
+      <mesh position={[0, 0, -1]} castShadow>
+        {/* Models are here */}
+
+        <Woman scale={0.5} />
+        <Peoples />
+      </mesh>
+      <Plane />
+      <OrbitControls />
+
+      <Environment background>
+        <mesh>
+          <sphereGeometry args={[-5, -5, 0]} />
+          <meshBasicMaterial color='#88b719' />
+        </mesh>
+      </Environment>
+    </Canvas>
   )
 }
 
--- src/Woman.jsx
+++ src/Woman.jsx
@@ -8,23 +8,25 @@ Title: Indian Office Woman
 
 import React, {useRef} from "react";
 import {useGLTF, useAnimations} from "@react-three/drei";
-// import {model} from "./models/woman.glb";
+import model from "./models/woman.glb";
 
 const Woman = (props) => {
    const group = useRef();
-   const {nodes, materials, animations} = useGLTF("./models/woman.glb");
+   const {nodes, materials, animations} = useGLTF(model);
    const {actions} = useAnimations(animations, group);
    return (
-      <group ref={group} {...props} dispose={null}>
+      <group ref={group} {...props} dispose={null} castShadow>
          <group name="Sketchfab_Scene">
             <group
                name="Sketchfab_model"
-               rotation={[-Math.PI / 2, 0, 0]}
+               rotation={[-Math.PI / 15, 0, 0]}
                scale={2.086}
+               castShadow
             >
                <group
                   name="7ab0120971d74f4ba654fc5c904dbf76fbx"
                   rotation={[Math.PI / 2, 0, 0]}
+                  castShadow
                >
                   <group name="Object_2">
                      <group name="RootNode">
@@ -36,72 +38,84 @@ const Woman = (props) => {
                                  geometry={nodes.Object_7.geometry}
                                  material={materials.AvatarBody}
                                  skeleton={nodes.Object_7.skeleton}
+                                 castShadow
                               />
                               <skinnedMesh
                                  name="Object_8"
                                  geometry={nodes.Object_8.geometry}
                                  material={materials.AvatarEyelashes}
                                  skeleton={nodes.Object_8.skeleton}
+                                 castShadow
                               />
                               <skinnedMesh
                                  name="Object_9"
                                  geometry={nodes.Object_9.geometry}
                                  material={materials.AvatarHead}
                                  skeleton={nodes.Object_9.skeleton}
+                                 castShadow
                               />
                               <skinnedMesh
                                  name="Object_10"
                                  geometry={nodes.Object_10.geometry}
                                  material={materials.AvatarLeftCornea}
                                  skeleton={nodes.Object_10.skeleton}
+                                 castShadow
                               />
                               <skinnedMesh
                                  name="Object_11"
                                  geometry={nodes.Object_11.geometry}
                                  material={materials.AvatarLeftEyeball}
                                  skeleton={nodes.Object_11.skeleton}
+                                 castShadow
                               />
                               <skinnedMesh
                                  name="Object_12"
                                  geometry={nodes.Object_12.geometry}
                                  material={materials.AvatarRightCornea}
                                  skeleton={nodes.Object_12.skeleton}
+                                 castShadow
                               />
                               <skinnedMesh
                                  name="Object_13"
                                  geometry={nodes.Object_13.geometry}
                                  material={materials.AvatarRightEyeball}
                                  skeleton={nodes.Object_13.skeleton}
+                                 castShadow
                               />
                               <skinnedMesh
                                  name="Object_14"
                                  geometry={nodes.Object_14.geometry}
                                  material={materials.AvatarTeethLower}
                                  skeleton={nodes.Object_14.skeleton}
+                                 castShadow
                               />
                               <skinnedMesh
                                  name="Object_15"
                                  geometry={nodes.Object_15.geometry}
                                  material={materials.AvatarTeethUpper}
                                  skeleton={nodes.Object_15.skeleton}
+                                 castShadow
                               />
                               <skinnedMesh
                                  name="Object_16"
                                  geometry={nodes.Object_16.geometry}
                                  material={materials.glasses}
                                  skeleton={nodes.Object_16.skeleton}
+                                 castShadow
                               />
                               <skinnedMesh
                                  name="Object_17"
                                  geometry={nodes.Object_17.geometry}
                                  material={materials.haircut}
                                  skeleton={nodes.Object_17.skeleton}
+                                 castShadow
                               />
                               <skinnedMesh
                                  name="Object_18"
                                  geometry={nodes.Object_18.geometry}
                                  material={materials.outfit}
                                  skeleton={nodes.Object_18.skeleton}
+                                 castShadow
                               />
                               <group name="AvatarBody" />
                               <group name="AvatarEyelashes" />
@@ -126,6 +140,6 @@ const Woman = (props) => {
    );
 };
 
-useGLTF.preload("./models/woman.glb");
+useGLTF.preload(model);
 
 export default Woman;
